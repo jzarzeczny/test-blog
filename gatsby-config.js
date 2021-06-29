e70af16cb856f6751871ea4772886bd68c9db40f
jzarzeczny <jkzarzeczny@gmail.com>
2021-06-29 14:09:16 +0200

    creating navbar

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: "psychodelikt",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: ["gatsby-plugin-gatsby-cloud", "gatsby-plugin-material-ui"],
 };
