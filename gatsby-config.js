0602684a4b096f2ea96de1e06698cec24ddbcbd6
jzarzeczny <jkzarzeczny@gmail.com>
2021-07-01 18:27:10 +0200

    fixing gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,6 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
-    "gatsby-plugin-material-ui",
     `gatsby-theme-material-ui`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
